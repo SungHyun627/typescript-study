8e3807b32ea4d0860277857b4eefffe2c84831de
Sunghyun627 <kksscc4@gmail.com>
2022-08-08 15:55:55 +0900

    feat: create blockain class

--- projects/typechain/src/index.ts
+++ projects/typechain/src/index.ts
@@ -21,3 +21,38 @@ class Block implements BlockShape {
     return crypto.createHash('sha256').update(toHash).digest('hex');
   }
 }
+
+class BlockChain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+
+  private getPrevHash() {
+    if (this.blocks.length === 0) return '';
+    // 마지막 block의 해쉬를 return
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+
+  public addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+
+  public getBlocks() {
+    return [...this.blocks];
+  }
+}
+
+const blockChain = new BlockChain();
+console.log(blockChain.getBlocks());
+
+blockChain.addBlock('first');
+blockChain.addBlock('second');
+blockChain.addBlock('third');
+
+console.log(blockChain.getBlocks());
